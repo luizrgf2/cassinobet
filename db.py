3f3b715e1836d3a77311f893b96954b7280509c3
luiz ricardo <luizrgfelipe@gmail.com>
2020-12-22 16:27:50 -0300

    bu fix

--- db.py
+++ db.py
@@ -5,6 +5,13 @@ cone = sqlite3.connect('roletas.db')
 
 cursor = cone.cursor()
 
-cursor.execute('SELECT * FROM Table1')
 
 
+#cursor.execute('CREATE TABLE teste1(id INTEGER AUTO_INCREMENT PRIMARY KEY, nome TEXT)')
+#cursor.execute('INSERT INTO teste1(nome) VALUES("luz fellipe")')
+#cursor.execute('CREATE TABLE roletas(id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), value VARCHAR(255))')
+cursor.execute(f'SELECT roleta FROM roletas WHERE name="Quantum Auto Roulette"')
+xy = cursor.fetchall()
+
+for x in xy:
+    print(x)
--- pegar_roletas.py
+++ pegar_roletas.py
@@ -5,12 +5,17 @@ from selenium.webdriver.common.keys import Keys
 import json
 import undetected_chromedriver
 from time import sleep as tm
+import sqlite3
+import _thread
 
 class Roll():
     
     def __init__(self,user:str,visible:bool):
         
         import platform
+
+        self.conec = sqlite3.connect('roletas.db')
+        self.cursor = self.conec.cursor()
         
         path = ''
         sistema = platform.system()
@@ -96,30 +101,134 @@ class Roll():
         for i in range(len(corte_principal_name)):
             
             names.append(corte_principal_name[i].split('">')[1].split('</div><div')[0])
+
+
+
+        
         
         return names
     def check_padroes(self):
         
-        text = str(self.driver.page_source)
-        open('tete.txt','w').write(text)
-        corte_principal = text.split('lobby-table-rol-round-result__container"><div')
-        corte_principal.pop(0)
-        nums = []
-        
-        
-        
+        while True:
+            text = str(self.driver.page_source)
+            open('tete.txt','w').write(text)
+            corte_principal = text.split('lobby-table-rol-round-result__container"><div')
+            corte_principal.pop(0)
+            nums = []
             
             
-        
-        for i in range(len(corte_principal)):
             
-            nums.append(str(corte_principal[i].split('item-number">')[1].split('</')[0])+' '+str(corte_principal[i].split('lobby-table-rol-round-result__item_')[1].split('">')[0]))
-        print(nums)
+                
+            names = self.get_names()        
+            for i in range(len(corte_principal)):
+                
+                nums.append(str(corte_principal[i].split('item-number">')[1].split('</')[0])+' '+str(corte_principal[i].split('lobby-table-rol-round-result__item_')[1].split('">')[0]))
+
+                conec = sqlite3.connect('roletas.db')
+                cursor = conec.cursor()
+                
+
+
+
+                cursor.execute(f'SELECT value FROM roletas WHERE name="{names[i]}"')
+
+                value_actual = str(cursor.fetchall())
+                value_actual = value_actual[3:len(value_actual)-4]
+                print(value_actual)
+                if value_actual != 'None':
+                    print(nums[i], value_actual, len(nums[i]), len(value_actual))
+                    if nums[i].find(value_actual) == -1 :
+                        
+
+                        cursor.execute(f'UPDATE roletas SET value="{nums[i]}" WHERE name="{names[i]}"')
+                        conec.commit()
+                        cursor.close()
+                        conec.close()
+
+                    else:
+                        cursor.execute(f'UPDATE roletas SET value="None" WHERE name="{names[i]}"')
+                        conec.commit()
+                        cursor.close()
+                        conec.close()
+                
+            
+    def alternada(self,giro:int):
+
+         while True:
+
+            
+            names = self.get_names()
+
+            for name in names:
+
+                self.cursor.execute(f'SELECT value FROM roletas WHERE name="{name}"')
+
+                value = str(self.cursor.fetchall())
+
+                value = value[3:len(value)-4]
+
+                if value != 'None':
+
+                    
+                    self.cursor.execute(f'SELECT roleta FROM roletas WHERE name="{name}"')
+                    roleta = str(self.cursor.fetchall())
+                    roleta = roleta[3:len(roleta)-4]
+                    rolete_final = ''
+                    if roleta == 'None':
+                        rolete_final = value
+                    else:
+                        rolete_final = roleta+','+value
+                    self.cursor.execute(f"UPDATE roletas SET roleta='{rolete_final}' WHERE name='{name}'")
+                
+                    self.conec.commit()
+
+                    self.cursor.execute(f'SELECT roleta FROM roletas WHERE name="{name}"')
+                    
+
+                    tm(0.3)            
+    def init(self):
+
+        self.entry_roletes()
+
+        _thread.start_new_thread(self.check_padroes,())
+        _thread.start_new_thread(self.detele_values,())
+
+        self.alternada(15)
+    def detele_values(self):
+
+        names = self.get_names()
+        conec = sqlite3.connect('roletas.db')
+        cursor = conec.cursor()
+
+        for name in names:
+            cursor.execute(f'SELECT roleta FROM roletas WHERE name="{name}"')
+            roleta = str(cursor.fetchall())
+            roleta = roleta[2:len(roleta)-3]
+
+            if roleta.find('None') == -1:
+
+
+                tamanho = len(roleta.split(','))-1
+
+                if tamanho >=20:
+
+            
+                    cursor.execute(f'UPDATE roletas SET roleta=Null WHERE name="{name}"')
+                    conec.commit()
         
-    def alternada(self,item:str):
-        
-        
-        
+
+
+
+
+
+tete = Roll('luizrgfg',True)
+tete.init()
+
+
+
+
+            
+
         
         
         
@@ -138,5 +247,4 @@ class Roll():
                 
 rol = Roll('luizrgfg',True)
 rol.entry_roletes()
-rol.check_padroes(14)
-
+rol.check_padroes()
